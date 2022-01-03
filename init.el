b3efa5cb1f260451e4f5d85f3c3187cdfdfb55aa
Hao Men <hao.men@gmail.com>
2022-01-03 14:32:47 -0500

    remove auto save temp mode

--- init.el
+++ init.el
@@ -37,10 +37,6 @@
    (quote
     (go-mode which-key all-the-icons neotree projectile doom-themes use-package evil))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
 
 ;; Visual effects
@@ -90,3 +86,6 @@
 ;; bracket auto-complete
 (electric-pair-mode 1)
 (setq electric-pair-preserve-balance nil)
+
+;; I absolutely need no backup files
+(setq make-backup-files nil)
