5952a2bb19c055b814fe84e9f47d99b6f6feab08
Hao Men cxxx][============/ <hao.men@gmail.com>
2023-07-14 22:16:15 -0400

    update the script

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-APP_LISTS="emacs vim gcc g++ clang python3 python3-pip htop screen tmux openssh-server git"
+APP_LISTS="emacs vim gcc g++ clang python3 python3-pip htop screen tmux openssh-server git curl"
 echo $APP_LISTS
 sudo apt install -y $APP_LISTS
 
 # Set my environment configs
 git clone https://github.com/haomen/config.git ~/git_config
 
-cp ~/git_config/.* ~/
+cp ~/git_config/.bashrc ~/
+cp ~/git_config/.screenrc ~/
+cp ~/git_config/.tmux.conf ~/
+cp ~/git_config/.emacs ~/
+cp ~/git_config/.gitconfig ~/
+cp ~/git_config/.gitignore_global ~/
 
 # install oh-my-zsh
-curl -s https://ohmyposh.dev/install.sh | bash -s
+curl -s https://ohmyposh.dev/install.sh | sudo bash -s
 git clone https://github.com/JanDeDobbeleer/oh-my-posh.git ~/.oh-my-posh
 
 # install python packages
 PYTHON_PACKAGES="python3-numpy python3-scipy python3-pandas python3-matplotlib"
 sudo apt install -y $PYTHON_PACKAGES
+
+# Refresh shell
+source ~/.bashrc
+
